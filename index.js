4ff5e5f744fad16edd0e705fd4a525bd34c508df
Sergio395 <tolser2019@gmail.com>
2025-04-03 10:59:47 -0300

    inicio db con index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const sqlite = require('sqlite3');
+let sql = "";
+const db = new sqlite.Database('./data.db', sqlite.OPEN_READWRITE, (error)=>{
+    if (error){
+        console.error(error);
+    }
+})
+
+sql = `CREATE TABLE IF NOT EXISTS categories(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(100) NOT NULL
+)`;
+
+// db.run(sql, (error)=>{
+//     if(error){
+//         console.error(error);
+//     }
+// })
+
+sql = `INSERT INTO categories (name) VALUES (?)`;
+
+db.run(sql,["Categoria 1"],(error)=>{
+    if(error){
+        console.error(error);
+    }
+    });
